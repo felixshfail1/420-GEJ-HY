e2e6dffea538e2e22daec6718f1f41ad79bd53d0
Hugo Lapointe Di Giacomo <hugo.lapo@gmail.com>
2019-01-25 20:59:50 -0500

    Dependencies Injection Example.

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(new InMemoryProductRepository().Products);
+    }
+}
---
+++ DIExample/DIExample/Models/InMemoryProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIExample.Models {
+    public class InMemoryProductRepository {
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public InMemoryProductRepository() {
+            new List<Product> {
+                new Product { Name = "R2D2", Price = 200000M },
+                new Product { Name = "WALL-E", Price = 50000M },
+                new Product { Name = "RobotCop", Price = 1000000M }
+            }.ForEach(p => AddProduct(p));
+        }
+
+        public Product this[string name] => products[name];
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product product) {
+            products[product.Name] = product;
+        }
+
+        public void RemoveProduct(Product product) {
+            products.Remove(product.Name);
+        }
+    }
+}
